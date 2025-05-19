cb02dc2618682f34aede9593c94c485b669ad619
WibiLaksono <wibilaksonowijaya@mail.ugm.ac.id>
2025-05-19 14:52:05 +0700

    initial setup for mobile

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,15 +1,11 @@
 import { Text, View } from "react-native";
+import './global.css'
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 items-center justify-center bg-green-200">
+      <Text className="text-red-500 text-xl">Edit app/index.tsx to edit this screen.</Text>
+      <Text className="text-blue-500 text-xl">Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
