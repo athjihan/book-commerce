641f0938d0b5f3a77999ccb71d49b283b603d3fc
WibiLaksono <wibilaksonowijaya@mail.ugm.ac.id>
2025-05-20 22:12:19 +0700

    ERROR FIX: Benerin error versi expo

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,11 +1,11 @@
 import { Text, View } from "react-native";
-import './global.css'
+import "./global.css"
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-green-200">
-      <Text className="text-red-500 text-xl">Edit app/index.tsx to edit this screen.</Text>
-      <Text className="text-blue-500 text-xl">Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 items-center justify-center bg-green-300">
+      <Text className="text-red-400">Edit app/index.tsx to edit this screen.</Text>
+      <Text className="text-blue-500">fix ea.</Text>
     </View>
   );
 }
