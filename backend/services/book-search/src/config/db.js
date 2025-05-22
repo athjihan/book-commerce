04a37e45aabdeffcfb7f790f514371bf26e30dc4
WibiLaksono <wibilaksonowijaya@mail.ugm.ac.id>
2025-05-23 00:31:23 +0700

    updte: set up second database dan ubah loading screen

--- backend/services/book-search/src/config/db.js
+++ backend/services/book-search/src/config/db.js
@@ -3,11 +3,17 @@ require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("✅ MongoDB Connected (Atlas)");
-  } catch (err) {
-    console.error("❌ MongoDB connection error:", err.message);
-    process.exit(1);
+    await mongoose.connect(process.env.MONGO_URI_PRIMARY);
+    console.log("✅ MongoDB Connected (Primary Atlas)");
+  } catch (errPrimary) {
+    console.error("❌ Failed to connect to PRIMARY:", errPrimary.message);
+    try {
+      await mongoose.connect(process.env.MONGO_URI_SECONDARY);
+      console.log("✅ MongoDB Connected (Secondary Atlas)");
+    } catch (errSecondary) {
+      console.error("❌ Failed to connect to SECONDARY:", errSecondary.message);
+      process.exit(1);
+    }
   }
 };
 
